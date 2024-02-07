17b7476ca893822d450b1a90a7852c971a61b865
walter_zero <249506369@qq.com>
2024-02-07 18:49:29 +0800

    添加客服微信

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
         AiMarkets
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+        customer service wx:newchat0619
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
